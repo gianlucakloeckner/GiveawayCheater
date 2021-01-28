e3ea0442d55bdafa19ae67c443481787d27fd70c
Gian-Luca Klöckner <gianlucakloeckner@yahoo.de>
2021-01-28 05:06:31 +0100

    Telegram Api Integration - Get New Messages - Decode JSON - Store Messages

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from instagram_private_api import Client, ClientCompatPatch
 import requests
-
+import json
 
 BotToken = '1633415315:AAE5UXzuUpQW9LlekvNVFsbMHUdWsV9TX74'
 
 global requestURL
 requestURL = 'http://api.telegram.org/bot' + BotToken + '/getUpdates'
 
-ResultRaw = requests.get(requestURL)
-Result = ResultRaw.json()
 
-print(Result)
+def GatherPost(messages):
+    messages = json.dumps(messages)
+    json_messages = json.loads(messages)
+
+    result = json_messages['result']
+
+
+
+
+    print(result)
+
+
+def GetTelegramMessages():
+    resultraw = requests.get(requestURL)
+    result = resultraw.json()
+    return result
+
+
+GatherPost(GetTelegramMessages())
