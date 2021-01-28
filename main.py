7093f96452c55a20941e1ff82c7681bed48badd9
Gian-Luca Klöckner <gianlucakloeckner@yahoo.de>
2021-01-28 04:14:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import telegram
+from instagram_private_api import Client, ClientCompatPatch
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+userid =
